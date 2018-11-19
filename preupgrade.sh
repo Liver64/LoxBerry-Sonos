c06d3b7d8738242c2597ec05aca4d0f572f3fa78
Liver64 <olewald64@gmail.com>
2018-11-19 21:32:02 +0100

    fixed upgrade

--- preupgrade.sh
+++ preupgrade.sh
@@ -61,8 +61,7 @@ echo "<INFO> Backing up existing MP3 files"
 cp -p -v -r $5/data/plugins/$3/ /tmp/$1\_upgrade/data
 
 echo "<INFO> Backing up existing Text files"
-cp -p -v -r $5/templates/plugins/$3/lang/t2s-text_de.ini /tmp/$1\_upgrade/templates
-cp -p -v -r $5/templates/plugins/$3/lang/t2s-text_en.ini /tmp/$1\_upgrade/templates
+cp -p -v -r $5/templates/plugins/$3/lang/t2s-text_*.* /tmp/$1\_upgrade/templates
 
 # Exit with Status 0
 exit 0
