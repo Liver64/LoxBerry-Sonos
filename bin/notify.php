a337ce4064fd14b6e60f490114f768a7dcac3e1a
Liver64 <olewald64@gmail.com>
2022-02-26 17:36:00 +0100

    v4.1.0

--- bin/notify.php
+++ bin/notify.php
@@ -1,12 +1,83 @@
 #!/usr/bin/env php
 <?php
+require_once "loxberry_system.php";
 require_once "loxberry_web.php";
 require_once "loxberry_log.php";
 
-#echo "<PRE>";
-$L = LBSystem::readlanguage("sonos.ini");
+$myFolder = "$lbpconfigdir";									// get config folder
+$myConfigFile = "player.cfg";									// get config file
+$configfile	= "/run/shm/s4lox_config.json";						// configuration file
 
-# Create an informational notification for the group "Sonos" (part of postupgradscript.sh)
-notify(LBPPLUGINDIR, "Sonos", $L['ERRORS.NOTE_UPGRADE']);
+# load Sonos Configuration
+	if (@!$data = file_get_contents($configfile)) {
+		$config = parseConfigFile();
+	} else {
+		$config = json_decode(file_get_contents($configfile), TRUE);
+	}
+	$sonoszonen = ($config['sonoszonen']);
+	$sonoszone = $sonoszonen;	
+	
+	$mainpl = array();
+	# check if MOVE or ROAM there
+	foreach ($sonoszonen as $zone => $player) {
+		$src = $sonoszonen[$zone][6];
+		if ($src == "on")   {
+			array_push($mainpl, $src);
+		}
+	}
+	if (count($mainpl) < 1)  {
+		# No Player marked
+		$L = LBSystem::readlanguage("sonos.ini");
+		# Create an informational notification for the group "Sonos" (part of postupgradscript.sh)
+		notify(LBPPLUGINDIR, "Sonos", $L['ERRORS.NOTE_UPGRADE']);
+		echo "<OK> Notify to update Config has been created";
+	} else {
+		echo "<OK> Player already marked, nothing to do";
+	}
+
+
+/**
+* Funktion : 	parseConfigFile --> backup falls die Configdatei nicht vorhanden ist
+*
+* @param: empty
+* @return: array($config)
+**/
+function parseConfigFile()    {
+	
+	global $master, $main, $zone, $ms, $batlevel, $config, $sonoszonen, $sonoszone, $myFolder;
+	
+	// Parsen der Konfigurationsdatei sonos.cfg
+	if (!file_exists($myFolder.'/sonos.cfg')) {
+		LOGWARN('bin/battery.php The file sonos.cfg could not be opened, please try again!');
+	} else {
+		$tmpsonos = parse_ini_file($myFolder.'/sonos.cfg', TRUE);
+		if ($tmpsonos === false)  {
+			#LOGERR('bin/battery.php The file sonos.cfg could not be parsed, the file may be disruppted. Please check/save your Plugin Config or check file "sonos.cfg" manually!');
+			exit(1);
+		}
+		#LOGDEB("bin/battery.php Sonos config has been loaded");
+	}
+	// Parsen der Sonos Zonen Konfigurationsdatei player.cfg
+	if (!file_exists($myFolder.'/player.cfg')) {
+		#LOGWARN('bin/battery.php: The file player.cfg could not be opened, please try again!');
+	} else {
+		$tmpplayer = parse_ini_file($myFolder.'/player.cfg', true);
+		if ($tmpplayer === false)  {
+			#LOGERR('bin/battery.php The file player.cfg could not be parsed, the file may be disrupted. Please check/save your Plugin Config or check file "player.cfg" manually!');
+			exit(1);
+		}
+		#LOGDEB("bin/battery.php Player config has been loaded");
+	}
+	$player = ($tmpplayer['SONOSZONEN']);
+	foreach ($player as $zonen => $key) {
+		$sonosnet[$zonen] = explode(',', $key[0]);
+	} 
+	$sonoszonen['sonoszonen'] = $sonosnet;
+	
+	// finale config für das Script
+	$config = array_merge($sonoszonen, $tmpsonos);
+	return $config;
+}
+
 
 ?>
